5a15250de7ccdd933e84e5b361dadbeecd1cb071
cuteprettykara <cuteprettykara@gmail.com>
2017-03-20 16:05:28 +0900

    MyBatis 프레임워크 2 - 설정 & Test

--- src/test/java/net/slipp/dao/MyBatisTest.java
+++ src/test/java/net/slipp/dao/MyBatisTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 
 import java.io.InputStream;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -12,6 +15,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import net.slipp.domain.users.User;
 
@@ -24,6 +30,22 @@ public class MyBatisTest {
 		String resource = "mybatis-config-test.xml";
 		InputStream inputStream = Resources.getResourceAsStream(resource);
 		sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		
+		// DB 생성 및 초기화
+		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+		populator.addScript(new ClassPathResource("slipp.sql"));
+		DatabasePopulatorUtils.execute(populator, getDataSource());
+		log.debug("database successfully initialized!");
+	}
+	
+	private DataSource getDataSource() {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:~/slipp");
+		dataSource.setUsername("sa");
+//		dataSource.setPassword("");
+		
+		return dataSource;
 	}
 
 	@Test
