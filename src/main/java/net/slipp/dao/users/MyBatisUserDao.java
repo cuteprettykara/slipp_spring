7ac19f91c34a2e429a1656ce76f55cadfbf19718
cuteprettykara <cuteprettykara@gmail.com>
2017-03-21 11:49:07 +0900

    component-scan 설정 추가

--- src/main/java/net/slipp/dao/users/MyBatisUserDao.java
+++ src/main/java/net/slipp/dao/users/MyBatisUserDao.java
@@ -1,14 +1,20 @@
 package net.slipp.dao.users;
 
+import javax.annotation.Resource;
+
 import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 
 import net.slipp.domain.users.User;
 
+@Repository("userDao")
 public class MyBatisUserDao implements UserDao {
 	private static final Logger log = LoggerFactory.getLogger(MyBatisUserDao.class);
 
+//	@Autowired
+	@Resource(name="sqlSession")
 	private SqlSession  sqlSession;
 	
 //	private DataSource dataSource;
